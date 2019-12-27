f85955d06d9bbb8cbb9333528efa09943a32f6e5
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-27 16:45:18 +1100

    add upvote api

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+
+const articlesInfor = {
+  'learn-react': {
+    upvotes: 0
+  },
+  'learn-node': {
+    upvotes: 0
+  },
+  'my-thoughts-on-resumes': {
+    upvotes: 0
+  }
+} 
+
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => res.send('Hello'));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
 
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+  articlesInfor[articleName].upvotes += 1;
+
+  res.status(200).send(`${articleName} now has ${articlesInfor[articleName].upvotes} upvotes.`);
+});
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
