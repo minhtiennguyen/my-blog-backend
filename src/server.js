3bee2fb8cea4a6ffc6cf206ed6f4a731f4f0e29d
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-27 16:59:10 +1100

    add api add-comment

--- src/server.js
+++ src/server.js
@@ -4,13 +4,16 @@ import bodyParser from 'body-parser';
 
 const articlesInfor = {
   'learn-react': {
-    upvotes: 0
+    upvotes: 0,
+    comments: []
   },
   'learn-node': {
-    upvotes: 0
+    upvotes: 0,
+    comments: []
   },
   'my-thoughts-on-resumes': {
-    upvotes: 0
+    upvotes: 0,
+    comments: []
   }
 } 
 
@@ -20,11 +23,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/api/articles/:name/upvote', (req, res) => {
-  const articleName = req.params.name;
+  const { name } = req.params;
 
-  articlesInfor[articleName].upvotes += 1;
+  articlesInfor[name].upvotes += 1;
 
-  res.status(200).send(`${articleName} now has ${articlesInfor[articleName].upvotes} upvotes.`);
+  res.status(200).send(`${name} now has ${articlesInfor[name].upvotes} upvotes.`);
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const { name } = req.params;
+  articlesInfor[name].comments.push({ username, text });
+  res.status(200).send(articlesInfor[name]);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
