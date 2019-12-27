754c894592d7a586c00ff67caf90ad75e348af92
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-27 17:44:21 +1100

    connect to mongodb

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import { MongoClient } from 'mongodb';
 
 const articlesInfor = {
   'learn-react': {
@@ -15,19 +15,40 @@ const articlesInfor = {
     upvotes: 0,
     comments: []
   }
-} 
-
+};
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+  const { name } = req.params;
+  const uri = 'mongodb://localhost:27017';
+
+  try {
+    const client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    const db = client.db('my-blog');
+    const articleInfo = await db.collection('articles').findOne({ name: name });
+
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+    client.close();
+  }
+});
 
 app.post('/api/articles/:name/upvote', (req, res) => {
   const { name } = req.params;
 
   articlesInfor[name].upvotes += 1;
 
-  res.status(200).send(`${name} now has ${articlesInfor[name].upvotes} upvotes.`);
+  res
+    .status(200)
+    .send(`${name} now has ${articlesInfor[name].upvotes} upvotes.`);
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
